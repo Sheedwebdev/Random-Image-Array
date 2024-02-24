0711495f533943fff60906a5807fabd0720e5181
Rashid Enahora <120060745+Sheedwebdev@users.noreply.github.com>
2024-02-23 22:03:52 -0500

    Display Random Image

--- js/script.js
+++ js/script.js
@@ -2,10 +2,37 @@ const button = document.querySelector("button");
 const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
+
 const getImage = async function () {
   const res = await fetch("http://picsum.photos/v2/list?limit=100");
-  const images = await res.json();
-  console.log(images);
+  const images = await res.json(); //  Declaring a variable that represents an array of objects (each object is an image object)
+  selectRandomImage(images);
+  // console.log(images);  Confirming if an array of objects (images) is displayed in the console
+  
 };
 
-getImage();
+const selectRandomImage = function (images) {
+  const randomIndex = Math.floor(Math.random() * images.length);  // Choosing a random index from 0 to 99
+  // console.log(randomIndex);  Confirming if a random index between 0 and 99 appears in the console
+  const randomImage = images[randomIndex];  // Declaring a variable that represents a random element (object) in the array of objects (named images) 
+  // console.log(randomImage);  // Confirming if a random object (image) is displayed in the console
+  displayImage(randomImage);
+};
+
+const displayImage = function (randomImage) {
+  const author = randomImage.author;
+  const imageAddress = randomImage.download_url;
+  authorSpan.innerText = author;
+  img.src = imageAddress;
+  imgDiv.classList.remove("hide");
+};
+
+button.addEventListener("click", function() {
+  getImage();
+});
+
+
+
+
+
+
