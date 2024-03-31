c77c9dc6b4effa875be4ca152533719f1c633c31
Rashid Enahora <120060745+Sheedwebdev@users.noreply.github.com>
2024-03-30 21:21:08 -0400

    Changed the fetch address to https

--- js/script.js
+++ js/script.js
@@ -1,34 +1,39 @@
-const button = document.querySelector("button");
-const authorSpan = document.querySelector(".author");
-const imgDiv = document.querySelector(".image-container");
-const img = document.querySelector(".img");
+// Part1: Declare all variables that will access elements on the webpage
+const button = document.querySelector("button"); //step1: 
+const authorSpan = document.querySelector(".author"); //step2:
+const imgDiv = document.querySelector(".image-container"); //step3:
+const img = document.querySelector(".img"); //step4:
 
-const getImage = async function () {
-  const res = await fetch("http://picsum.photos/v2/list?limit=100");
-  const images = await res.json(); //  Declaring a variable that represents an array of objects (each object is an image object)
-  selectRandomImage(images);
-  // console.log(images);  Confirming if an array of objects (images) is displayed in the console
-  
+//  Part2: Declare a variable for the asynchronous function 
+const getImage = async function () { //step4:
+  const res = await fetch("https://picsum.photos/v2/list?limit=100"); //step5: Declare a variable for that represents fetching the data from the photos API
+  const images = await res.json(); //step6: Declaring a variable that represents interpreting that data as an array of objects (each object is an image object)
+  //console.log(images);  //step7: Confirming if an array of objects (images) is displayed in the console
+  selectRandomImage(images); //step13: Concurrently randomly selects and displays an image from the array of images 
 };
 
-const selectRandomImage = function (images) {
-  const randomIndex = Math.floor(Math.random() * images.length);  // Choosing a random index from 0 to 99
-  // console.log(randomIndex);  Confirming if a random index between 0 and 99 appears in the console
-  const randomImage = images[randomIndex];  // Declaring a variable that represents a random element (object) in the array of objects (named images) 
-  // console.log(randomImage);  // Confirming if a random object (image) is displayed in the console
-  displayImage(randomImage);
+//  Part3: Declare a variable that will select a random image
+const selectRandomImage = function (images) { //step8:
+  const randomIndex = Math.floor(Math.random() * images.length);  //step9: Choosing a random index from 0 to 99
+  //console.log(randomIndex);  //step10: Confirming if a random index between 0 and 99 appears in the console
+  const randomImage = images[randomIndex];  //step11: Declaring a variable that represents a random element (object) in the array of objects (named images) 
+  // console.log(randomImage);  //step12: Confirming if a random object (image) is displayed in the console
+  displayImage(randomImage); //step20: displaying an image with author info
 };
 
-const displayImage = function (randomImage) {
-  const author = randomImage.author;
-  const imageAddress = randomImage.download_url;
-  authorSpan.innerText = author;
-  img.src = imageAddress;
-  imgDiv.classList.remove("hide");
+
+// Part4: Declare a variable that will display the selected random image
+const displayImage = function (randomImage) { //step14:
+  const author = randomImage.author; //step15:
+  const imageAddress = randomImage.download_url; //step16:
+  authorSpan.innerText = author; //step17:
+  img.src = imageAddress; //step18:
+  imgDiv.classList.remove("hide"); //step19:
 };
 
-button.addEventListener("click", function() {
-  getImage();
+// Part5: Create a click event that will display the random image
+button.addEventListener("click", function() { //step21:
+  getImage(); //step22:
 });
 
 
